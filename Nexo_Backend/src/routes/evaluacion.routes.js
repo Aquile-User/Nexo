2fbc9af6627dc2709879046105c3e2dd57a5e043
Aquile-User <Aquilenyisuero@gmail.com>
2025-03-25 11:57:52 -0400

    Agregar nueva ruta para evaluaciones en la configuración de la API

---
+++ Nexo_Backend/src/routes/evaluacion.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const evaluacionController = require("../controllers/evalucacion.controllers");
+
+router.get("/:id?", evaluacionController.obtenerEvaluaciones);
+router.post("/", evaluacionController.crearEvaluacion);
+router.put("/:id", evaluacionController.actualizarEvaluacion);
+router.delete("/:id", evaluacionController.eliminarEvaluacion);
+router.patch("/:id", evaluacionController.cancelarEvaluacion);
+
+module.exports = router;
+
